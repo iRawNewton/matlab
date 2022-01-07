a4b19721848b815eceba81bd64fde42fff194804
Gaurab Roy <61748941+iRawNewton@users.noreply.github.com>
2022-01-07 11:12:26 +0530

    Add files via upload

---
+++ files/MATLAB/add_subOnMatrix.m
@@ -0,0 +1,13 @@
+%create two 3 by 3 matrix
+%a = 10 20 30; 20 40 50; 5 4 7
+%b = 1 2 3; 5 7 9; 2 2 2
+%perform addtion and substraction on the matrix
+
+a = [10 20 30; 20 40 50; 5 4 7];
+b = [1 2 3; 5 7 9; 2 2 2];
+c = a + b
+d = a - b
+
+e = a .* b
+f = a ./ b
+f = a .\ b
